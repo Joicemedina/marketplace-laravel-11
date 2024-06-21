817e06fd5a39904c295fa5bfc4dfc86b9f1e2cd5
joice <catalowayempresas@gmail.com>
2024-06-21 14:46:50 -0300

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        'DB'::table('users')->insert([
+            [
+                'name' => 'Joice medina',
+                'username' => 'cataloway',
+                'email' => 'catalowayempresas@gmail.com',
+                'role' => 'admin',
+                'status' => 'active',
+                'password' => bcrypt('password')
+            ],
+            [
+                'name' => 'Vendedor vendor',
+                'username' => 'vendor',
+                'email' => 'vendedorempresas@gmail.com',
+                'role' => 'vendor',
+                'status' => 'active',
+                'password' => bcrypt('password')
+            ],
+            [
+                'name' => 'User name',
+                'username' => 'user',
+                'email' => 'clientesempresas@gmail.com',
+                'role' => 'user',
+                'status' => 'active',
+                'password' => bcrypt('password')
+            ]
+        ]);
+    }
+}
